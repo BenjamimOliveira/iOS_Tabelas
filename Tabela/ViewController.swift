270b6abbdd9c4ce0ecc6882023c7c542f047875e
Aluno <aluno@imac04-2.local>
2019-05-07 17:58:29 +0100

    Tabela

--- Tabela/ViewController.swift
+++ Tabela/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, UITableViewDataSource {
 
+    var array = ["Viana", "Braga", "Barcelos", "Porto"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,9 +20,21 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-        //aegsdgsgss
+        
+        
     }
 
-
+    //MARK: UiTableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: "Cell")
+        cell.textLabel?.text = array[indexPath.row]
+        cell.detailTextLabel?.text = "Texto Extra"
+        return cell
+    }
+    
 }
 
