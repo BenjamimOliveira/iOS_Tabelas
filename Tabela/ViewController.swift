0e0751c1c2d1f07b7befe408e7c5e9e6108402d1
Aluno <aluno@imac04-2.local>
2019-05-07 23:57:04 +0100

    Navigation Controller

--- Tabela/ViewController.swift
+++ Tabela/ViewController.swift
@@ -18,9 +18,14 @@ class ViewController: UIViewController, UITextFieldDelegate, UITableViewDataSour
     }
     
     //MARK: Atributtes
-    
     @IBOutlet weak var tableView1: UITableView!
     @IBOutlet weak var tableView2: UITableView!
+    
+    //MARK: Actions
+    
+    @IBAction func butActionValidar(_ sender: Any) {
+        verifyChosen()
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -75,10 +80,21 @@ class ViewController: UIViewController, UITextFieldDelegate, UITableViewDataSour
             }
             teste.backgroundColor = UIColor.green
             return [editar, delete, teste]
-        }
+        } else {
+            let editar = UITableViewRowAction(style: .default, title: "Editar"){action,index in
+                print("Editar: " + String(index.row) + " " + self.array[index.row])
+            }
             
-            return [UITableViewRowAction(), UITableViewRowAction()]
+            editar.backgroundColor = UIColor.blue
+            let delete = UITableViewRowAction(style: .default, title: "Apagar"){action,index in
+                print("Apagar: " + String(index.row) + " " + self.array[index.row])
+            }
+            delete.backgroundColor = UIColor.red
+            return [editar, delete]
+        }
+        
     }
+    
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if arrayB[indexPath.row] {
@@ -88,7 +104,6 @@ class ViewController: UIViewController, UITextFieldDelegate, UITableViewDataSour
             tableView.cellForRow(at: indexPath as IndexPath)?.accessoryType = .checkmark
             arrayB[indexPath.row] = true
         }
-        verifyChosen()
     }
     
     func verifyChosen(){
@@ -99,10 +114,5 @@ class ViewController: UIViewController, UITextFieldDelegate, UITableViewDataSour
             }
         }
     }
-    
+
 }
-
-
-
-
-
